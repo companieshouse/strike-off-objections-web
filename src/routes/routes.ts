1d1691ba1bee56566cbb57df9af98224fdcf0274
markpit <mpitten@companieshouse.gov.uk>
2020-06-11 09:36:27 +0100

    BI-3868 Changing license to MIT

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 
 const router: Router = Router();
 
-router.get("/", (req, response) => response.send("This is the Strike Off Objections app &#128516;"));
+router.get("/", (req, response) => response.send("This is the Strike Off Objections app"));
 
 export default router;
