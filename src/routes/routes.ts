0bb23f28aa235e8d24e3a223010a156a6bc981ea
mwestacott <mwestacott1@companieshouse.gov.uk>
2020-06-12 11:57:56 +0100

    BI-3927 Add a new 'What is the company number?' screen

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,20 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
+import * as pageURLs from "../model/page.urls";
+import { Templates } from "../model/template.paths";
 
 const router: Router = Router();
 
+/**
+ * Simply renders a view template.
+ *
+ * @param template the template name
+ */
+const renderTemplate = (template: string) => (req: Request, res: Response, next: NextFunction) => {
+  return res.render(template, { templateName: template });
+};
+
 router.get("/", (req, response) => response.send("This is the Strike Off Objections app"));
+
+router.get(pageURLs.COMPANY_NUMBER, renderTemplate(Templates.COMPANY_NUMBER));
 
 export default router;
