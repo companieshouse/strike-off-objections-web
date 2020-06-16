6afcc1bff2218e4917e2291a02721b570547faa8
Matthew Culpin <mculpin@companieshouse.gov.uk>
2020-06-16 09:32:03 +0100

    indentation

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response, Router } from "express";
 const router: Router = Router();
 
 const renderTemplate = (template: string) => (req: Request, res: Response, next: NextFunction) => {
-    return res.render(template, { templateName: template });
+  return res.render(template, { templateName: template });
 };
 
 router.get("/", renderTemplate("index"));
