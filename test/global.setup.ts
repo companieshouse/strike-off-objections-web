f34a92485d8ff7be761a6af6c448aebb58b6917d
ICoxCH <ICox@companieshouse.gov.uk>
2020-06-18 12:00:20 +0100

    Added test

--- test/global.setup.ts
+++ test/global.setup.ts
@@ -1,3 +1,6 @@
 export default async () => {
   process.env.COOKIE_NAME = "cookie_name";
+  process.env.COOKIE_DOMAIN = "cookie_domain";
+  process.env.COOKIE_SECRET = "123456789012345678901234";
+  process.env.CACHE_SERVER = "cache_server";
 };
---
+++ test/signInMiddleware/sign.in.spec.unit.ts
@@ -0,0 +1,21 @@
+import app from "../../src/app";
+import * as request from "supertest";
+
+describe("Sign in middleware tests", () => {
+
+  it("should redirect to sign in page with no cookie", async () => {
+
+    const response = await request(app)
+      .get("/strike-off-objections/company_number");
+
+    expect(response.status).toEqual(302);
+  });
+
+  it("should not redirect to sign in page with cookie", async () => {
+    const response = await request(app)
+      .get("/strike-off-objections/company_number");
+
+    expect(response.status).toEqual(200);
+  });
+
+});
