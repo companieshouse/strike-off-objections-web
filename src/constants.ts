7f3ee6f09590de6ab6533ff35702515255a32a7c
Matthew Culpin <mculpin@companieshouse.gov.uk>
2020-06-24 14:58:00 +0100

    refactored keys tring into a constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const APP_NAME = "strike-off-objections-web";
 export const OBJECTIONS_SESSION = "objections_session";
+export const OBJECTIONS_COMPANY_PROFILE = "objections_company_profile";
--- src/controllers/confirm.company.controller.ts
+++ src/controllers/confirm.company.controller.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { OBJECTIONS_COMPANY_PROFILE } from "../constants";
 import { Templates } from "../model/template.paths";
 import { getValueFromObjectionsSession } from "../services/objections.session.service";
 import logger from "../utils/logger";
@@ -12,7 +13,7 @@ import logger from "../utils/logger";
 
 export const route = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     if (req.session && req.session.data) {
-        const company: string = getValueFromObjectionsSession(req.session, "company_data");
+        const company: string = getValueFromObjectionsSession(req.session, OBJECTIONS_COMPANY_PROFILE);
         return res.render(Templates.CONFIRM_COMPANY, {
             company,
             templateName: Templates.CONFIRM_COMPANY,
