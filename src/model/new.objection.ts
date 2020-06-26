5e985c2554ecf7a96f476c4f2d3bc841da461b1a
mwestacott <mwestacott1@companieshouse.gov.uk>
2020-06-26 13:34:05 +0100

    BI-4115 Remove object and just return id as string

--- src/model/new.objection.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Represents the JSON object returned from the Objections API when a new objection is initially created.
- */
-export default interface NewObjection {
-  objectionId: string;
-}
--- src/services/objections.api.service.ts
+++ src/services/objections.api.service.ts
@@ -1,4 +1,3 @@
-import NewObjection from "../model/new.objection";
 import logger from "../utils/logger";
 
 /**
@@ -9,15 +8,13 @@ import logger from "../utils/logger";
  *
  * @returns the id of the newly created objection
  */
-export const createNewObjection = async (companyNumber: string, token: string): Promise<NewObjection> => {
+export const createNewObjection = (companyNumber: string, token: string): string => {
 
   logger.debug(`Creating a new objection for company number ${companyNumber}`);
 
   // TODO Call the actual Objections API (via the SDK Node API?) when end-point is implemented. Covered by
   //      JIRA sub-task BI-4121
 
-  return {
-    // Until API is called to return an objection id, just generate a random 6 digit number and set that
-    objectionId: String(Math.floor(100000 + Math.random() * 900000)),
-  };
+  // Until API is called to return an objection id, just generate a random 6 digit number
+  return String(Math.floor(100000 + Math.random() * 900000));
 };
