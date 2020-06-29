78edd71ff95b69768ec0a765f76bd4a0c0a824f6
Svarallo <Svarallo@companieshouse.gov.uk>
2020-06-29 16:48:47 +0100

    Removed async on enter information route

--- src/controllers/enter.information.controller.ts
+++ src/controllers/enter.information.controller.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { Templates } from "../model/template.paths";
 
-const route = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+const route = (req: Request, res: Response, next: NextFunction) => {
   return res.render(Templates.ENTER_INFORMATION, {
     templateName: Templates.ENTER_INFORMATION,
   });
