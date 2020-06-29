805e39e1aada0789edaa0d4b66e088f7e97607dc
markpit <mpitten@companieshouse.gov.uk>
2020-06-29 17:32:00 +0100

    BI-4123 Changing objections.api.service imports back to individual imports

--- test/services/objections.api.service.spec.unit.ts
+++ test/services/objections.api.service.spec.unit.ts
@@ -1,4 +1,4 @@
-import * as objectionsApi from "../../src/services/objections.api.service";
+import { createNewObjection, ObjectionPatch, ObjectionStatus, patchObjection } from "../../src/services/objections.api.service";
 
 const ACCESS_TOKEN = "KGGGUYUYJHHVK1234";
 const COMPANY_NUMBER = "00006400";
@@ -6,18 +6,18 @@ const COMPANY_NUMBER = "00006400";
 describe("objections API service unit tests", () => {
 
   it("returns an id when a new objection is created", () => {
-    const objectionId = objectionsApi.createNewObjection(COMPANY_NUMBER, ACCESS_TOKEN);
+    const objectionId = createNewObjection(COMPANY_NUMBER, ACCESS_TOKEN);
 
     expect(objectionId).toBeDefined();
     expect(typeof objectionId).toBe("string");
   });
 
   it("returns undefined when patching an objection", () => {
-    const patch: objectionsApi.ObjectionPatch = {
+    const patch: ObjectionPatch = {
       reason: "some reason or other",
-      status: objectionsApi.ObjectionStatus.SUBMITTED,
+      status: ObjectionStatus.SUBMITTED,
     };
-    const patchResult = objectionsApi.patchObjection(COMPANY_NUMBER, ACCESS_TOKEN, patch);
+    const patchResult = patchObjection(COMPANY_NUMBER, ACCESS_TOKEN, patch);
     expect(patchResult).toBeUndefined();
   });
 });
