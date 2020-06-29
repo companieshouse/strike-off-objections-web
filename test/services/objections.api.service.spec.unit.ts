1ebee46221fc7c1fbaa2d3a66c666fcccb375168
markpit <mpitten@companieshouse.gov.uk>
2020-06-29 17:00:32 +0100

    BI-4123 Adding PATCH stub to objections.api.service

--- test/services/objections.api.service.spec.unit.ts
+++ test/services/objections.api.service.spec.unit.ts
@@ -1,13 +1,23 @@
-import { createNewObjection } from "../../src/services/objections.api.service";
+import * as objectionsApi from "../../src/services/objections.api.service";
 
 const ACCESS_TOKEN = "KGGGUYUYJHHVK1234";
+const COMPANY_NUMBER = "00006400";
 
 describe("objections API service unit tests", () => {
 
   it("returns an id when a new objection is created", () => {
-    const objectionId = createNewObjection("00006400", ACCESS_TOKEN);
+    const objectionId = objectionsApi.createNewObjection(COMPANY_NUMBER, ACCESS_TOKEN);
 
     expect(objectionId).toBeDefined();
     expect(typeof objectionId).toBe("string");
+  });
+
+  it("returns undefined when patching an objection", () => {
+    const patch: objectionsApi.ObjectionPatch = {
+      reason: "some reason or other",
+      status: objectionsApi.ObjectionStatus.SUBMITTED,
+    };
+    const patchResult = objectionsApi.patchObjection(COMPANY_NUMBER, ACCESS_TOKEN, patch);
+    expect(patchResult).toBeUndefined();
   });
 });
