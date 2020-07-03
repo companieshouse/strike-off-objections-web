4e8f4e46d9c91b5efa40dd4c59c6a215db2d87c4
ICoxCH <ICox@companieshouse.gov.uk>
2020-07-03 17:22:03 +0100

    updated test names to reflect new behaviour

--- test/services/objections.session.service.spec.unit.ts
+++ test/services/objections.session.service.spec.unit.ts
@@ -1,5 +1,4 @@
 import { Session } from "ch-node-session-handler";
-import ObjectionCompanyProfile from "../../src/model/objection.company.profile";
 import { retrieveAccessTokenFromSession } from "../../src/services/objections.session.service";
 
 const accessTokenValue = "tokenABC123";
@@ -20,14 +19,14 @@ describe ("objections session service tests", () => {
     expect(token).toEqual(accessTokenValue);
   });
 
-  it("should receive undefined when token is absent", () => {
+  it("should throw error when token is absent", () => {
     const session: Session = new Session();
     expect(() => {
       retrieveAccessTokenFromSession(session);
     }).toThrow();
   });
 
-  it("should receive undefined when token is empty", () => {
+  it("should throw error when token is empty", () => {
     const session: Session = new Session();
     session.data = {
       signin_info: {
@@ -39,16 +38,3 @@ describe ("objections session service tests", () => {
     }).toThrow();
   });
 });
-
-const dummyCompanyProfile: ObjectionCompanyProfile = {
-  address: {
-    line_1: "line1",
-    line_2: "line2",
-    postCode: "post code",
-  },
-  companyName: "Girls school trust",
-  companyNumber: "00006400",
-  companyStatus: "Active",
-  companyType: "limited",
-  incorporationDate: "26 June 1872",
-};
