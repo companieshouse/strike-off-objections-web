f3036a051b337cc42131a8c3d4f6935416144d76
Matthew Culpin <mculpin@companieshouse.gov.uk>
2020-07-15 15:39:07 +0100

    OBJ-13 remove screen display filename

--- src/controllers/remove.document.controller.ts
+++ src/controllers/remove.document.controller.ts
@@ -1,6 +1,18 @@
+import { Session } from "ch-node-session-handler";
 import { NextFunction, Request, Response } from "express";
 import { Templates } from "../model/template.paths";
+import { Attachment } from "../modules/sdk/objections";
+import * as objectionService from "../services/objection.service";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
-  return res.render(Templates.REMOVE_DOCUMENT);
+  const attachments: Attachment[] = await objectionService.getAttachments(req.session as Session);
+  const attachment = attachments.filter((attachmentItem) => attachmentItem.id === req.query.documentID).pop();
+  if (attachment) {
+    return res.render(Templates.REMOVE_DOCUMENT, {
+      fileName: attachment.name,
+      templateName: Templates.REMOVE_DOCUMENT,
+    });
+  } else {
+    return res.render(Templates.REMOVE_DOCUMENT);
+  }
 };
---
+++ test/controller/document_upload/remove.document.controller.spec.unit.ts
@@ -0,0 +1,82 @@
+jest.mock("ioredis");
+jest.mock("../../../src/middleware/authentication.middleware");
+jest.mock("../../../src/middleware/session.middleware");
+jest.mock("../../../src/middleware/objection.session.middleware");
+jest.mock("../../../src/services/objection.session.service");
+jest.mock("../../../src/services/objection.service");
+
+import { Session } from "ch-node-session-handler";
+import { NextFunction, Request, Response } from "express";
+import request from "supertest";
+import app from "../../../src/app";
+import { OBJECTIONS_SESSION_NAME } from "../../../src/constants";
+import authenticationMiddleware from "../../../src/middleware/authentication.middleware";
+import objectionSessionMiddleware from "../../../src/middleware/objection.session.middleware";
+import sessionMiddleware from "../../../src/middleware/session.middleware";
+import * as pageURLs from "../../../src/model/page.urls";
+import { getAttachments } from "../../../src/services/objection.service";
+import { COOKIE_NAME } from "../../../src/utils/properties";
+
+const QUERY_ID: string = "?documentID=attachment1";
+const ATTACHMENT_ID = "sghsaghj-3623-khh";
+const TEXT_FILE_NAME = "text.txt";
+const dummySession: Session = {
+    data: {},
+} as Session;
+
+const dummyAttachments = [
+ {
+   id: ATTACHMENT_ID,
+   name: TEXT_FILE_NAME,
+ },
+];
+
+const mockSessionMiddleware = sessionMiddleware as jest.Mock;
+mockSessionMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => {
+    req.session = dummySession;
+    return next();
+});
+
+const mockAuthenticationMiddleware = authenticationMiddleware as jest.Mock;
+mockAuthenticationMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next() );
+
+const mockObjectionSessionMiddleware = objectionSessionMiddleware as jest.Mock;
+mockObjectionSessionMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => {
+    if (req.session) {
+        req.session.data[OBJECTIONS_SESSION_NAME] = { };
+        return next();
+    }
+});
+
+const mockGetAttachments = getAttachments as jest.Mock;
+
+describe("remove document url tests", () => {
+
+    beforeEach(() => {
+      mockGetAttachments.mockReset().mockImplementation(() => dummyAttachments);
+    });
+
+    it ("should find remove document page with get", async () => {
+        const res = await request(app)
+            .get(pageURLs.OBJECTIONS_REMOVE_DOCUMENT + QUERY_ID)
+            .set("Referer", "/")
+            .set("Cookie", [`${COOKIE_NAME}=123`]);
+        expect(res.status).toEqual(200);
+    });
+    it ("should return 404 if remove document page with put", async () => {
+
+        const res = await request(app)
+            .put(pageURLs.OBJECTIONS_REMOVE_DOCUMENT + QUERY_ID)
+            .set("Referer", "/")
+            .set("Cookie", [`${COOKIE_NAME}=123`]);
+        expect(res.status).toEqual(404);
+    });
+    it ("should find remove document page when attachments are missing", async () => {
+        mockGetAttachments.mockReset().mockImplementation(() => []);
+        const res = await request(app)
+            .get(pageURLs.OBJECTIONS_REMOVE_DOCUMENT + QUERY_ID)
+            .set("Referer", "/")
+            .set("Cookie", [`${COOKIE_NAME}=123`]);
+        expect(res.status).toEqual(200);
+    });
+});
