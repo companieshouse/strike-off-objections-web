cd6635d91828beb866ba1d38365efa4f94480285
Matthew Culpin <mculpin@companieshouse.gov.uk>
2020-07-15 13:02:21 +0100

    more spacing

--- src/controllers/remove.document.controller.ts
+++ src/controllers/remove.document.controller.ts
@@ -2,5 +2,5 @@ import { NextFunction, Request, Response } from "express";
 import { Templates } from "../model/template.paths";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
- return res.render(Templates.REMOVE_DOCUMENT);
+  return res.render(Templates.REMOVE_DOCUMENT);
 };
