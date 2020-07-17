527f94e5e7539367f6bc053dd95973899e485a7a
ICoxCH <ICox@companieshouse.gov.uk>
2020-07-17 12:15:26 +0100

    Remove document no journey

--- src/controllers/remove.document.controller.ts
+++ src/controllers/remove.document.controller.ts
@@ -1,8 +1,12 @@
 import { Session } from "ch-node-session-handler";
 import { NextFunction, Request, Response } from "express";
+import { OBJECTIONS_DOCUMENT_UPLOAD } from "../model/page.urls";
 import { Templates } from "../model/template.paths";
 import { Attachment } from "../modules/sdk/objections";
 import { getAttachment } from "../services/objection.service";
+import logger from "../utils/logger";
+
+const REMOVE_DOCUMENT_FORM_FIELD: string = "removeDocument";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
   const session: Session = req.session as Session;
@@ -17,5 +21,14 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
     } else {
       return res.render(Templates.REMOVE_DOCUMENT);
     }
+  }
+};
+
+export const post = async (req: Request, res: Response, next: NextFunction) => {
+  if (req.body[REMOVE_DOCUMENT_FORM_FIELD] === "yes") {
+    logger.infoRequest(req, "Yes journey not yet implemented");
+    return next(new Error("Yes journey not yet implemented"));
+  } else {
+    return res.redirect(OBJECTIONS_DOCUMENT_UPLOAD);
   }
 };
