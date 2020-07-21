bd1abee04346fd7212e32b54a114f02f247d6f9d
markpit <mpitten@companieshouse.gov.uk>
2020-07-21 13:45:25 +0100

    OBJ-11 adding logging to remove document page

--- src/controllers/remove.document.controller.ts
+++ src/controllers/remove.document.controller.ts
@@ -4,15 +4,19 @@ import { OBJECTIONS_DOCUMENT_UPLOAD } from "../model/page.urls";
 import { Templates } from "../model/template.paths";
 import { Attachment } from "../modules/sdk/objections";
 import { deleteAttachment, getAttachment } from "../services/objection.service";
+import logger from "../utils/logger";
 
 const REMOVE_DOCUMENT_FORM_FIELD: string = "removeDocument";
 const ATTACHMENT_ID_FORM_FIELD: string = "attachmentId";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    logger.debugRequest(req, "Getting remove document page");
     const session: Session = req.session as Session;
     const attachmentId: string = req.query.documentID as string;
     const attachment: Attachment = await getAttachment(session, attachmentId);
+    logger.debugRequest(req,
+                        `Showing remove documents page for attachmentId ${attachmentId}, name: ${attachment.name}`);
 
     return res.render(Templates.REMOVE_DOCUMENT, {
         attachmentId,
@@ -20,15 +24,18 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
         templateName: Templates.REMOVE_DOCUMENT,
       });
   } catch (e) {
+    logger.errorRequest(req, e.message);
     return next(e);
   }
 };
 
 export const post = async (req: Request, res: Response, next: NextFunction) => {
+  logger.debugRequest(req, "POST remove document received");
   if (req.body[REMOVE_DOCUMENT_FORM_FIELD] === "yes") {
     const session: Session = req.session as Session;
     const attachmentId: string = req.body[ATTACHMENT_ID_FORM_FIELD];
     if (attachmentId) {
+      logger.debugRequest(req, `Removing document with attachmentId ${attachmentId}`);
       await deleteAttachment(session, attachmentId);
       return res.redirect(OBJECTIONS_DOCUMENT_UPLOAD);
     } else {
