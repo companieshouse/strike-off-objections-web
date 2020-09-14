87b297f2c5a7516e6d308c0581ac6bc69df88408
Svarallo <Svarallo@companieshouse.gov.uk>
2020-09-14 18:31:30 +0100

    addressing comments and moving validation handler to seperate method

--- src/controllers/objecting.entity.name.controller.ts
+++ src/controllers/objecting.entity.name.controller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { check, ValidationError, validationResult } from "express-validator";
+import { check, Result, ValidationError, validationResult } from "express-validator";
 import { ErrorMessages } from "../model/error.messages";
 import { createGovUkErrorData, GovUkErrorData } from "../model/govuk.error.data";
 import { OBJECTIONS_COMPANY_NUMBER } from "../model/page.urls";
@@ -8,8 +8,8 @@ import { ObjectionCreate } from "../modules/sdk/objections";
 import { Session } from "ch-node-session-handler";
 import { Templates } from "../model/template.paths";
 
-const FULL_NAME_FIELD: string = "fullName";
-const DIVULGE_INFO_FIELD: string = "shareIdentity";
+const FULL_NAME_FIELD = "fullName";
+const DIVULGE_INFO_FIELD = "shareIdentity";
 
 const validators = [
   check(FULL_NAME_FIELD).not().isEmpty().withMessage(ErrorMessages.ENTER_FULL_NAME),
@@ -17,50 +17,50 @@ const validators = [
 ];
 
 /**
- * ProcessForm validates input and processes form
+ * POST validates input and processes form
  * @param req
  * @param res
  * @param next
  */
-export const processForm = [...validators, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+export const post = [...validators, (req: Request, res: Response, next: NextFunction) => {
   const errors = validationResult(req);
-  const errorListData: GovUkErrorData[] = [];
   if (!errors.isEmpty()) {
-    let objectingEntityNameErr: GovUkErrorData | undefined;
-    let shareIdentityErr: GovUkErrorData | undefined;
-
-    errors.array({ onlyFirstError: true })
-      .forEach((valErr: ValidationError) => {
-        const govUkErrorData: GovUkErrorData = createGovUkErrorData(valErr.msg, "#" + valErr.param, true, "");
-        switch ((valErr.param)) {
-            case FULL_NAME_FIELD:
-              objectingEntityNameErr = govUkErrorData;
-              break;
-            case DIVULGE_INFO_FIELD:
-              shareIdentityErr = govUkErrorData;
-              break;
-        }
-
-        errorListData.push(govUkErrorData);
-      });
-    return res.render(Templates.OBJECTING_ENTITY_NAME, {
-      shareIdentityErr,
-      errorList: errorListData,
-      objectingEntityNameErr,
-      templateName: Templates.OBJECTING_ENTITY_NAME,
-    });
+    return showErrorsOnScreen(errors, res);
+  }
+  const fullNameValue: string = req.body.fullName;
+  const shareIdentityValue: boolean = req.body.shareIdentity === "yes";
+  const objectionCreate: ObjectionCreate = { shareIdentity: shareIdentityValue, fullName: fullNameValue };
+  const session: Session | undefined  = req.session;
+  if (session) {
+    addObjectionCreateToObjectionSession(session, objectionCreate);
+    return res.redirect(OBJECTIONS_COMPANY_NUMBER);
   } else {
-    const fullNameValue: string = req.body.fullName;
-    const shareIdentityValue: boolean = req.body.shareIdentity === "yes";
-    const objectionCreate: ObjectionCreate = { shareIdentity: shareIdentityValue, fullName: fullNameValue };
-
-    const session: Session | undefined  = req.session;
-    if (session) {
-      addObjectionCreateToObjectionSession(session, objectionCreate);
-      return res.redirect(OBJECTIONS_COMPANY_NUMBER);
-    } else {
-      const error: Error = new Error("Session not present");
-      next(error);
-    }
+    const error: Error = new Error("Session not present");
+    next(error);
   }
 }];
+
+const showErrorsOnScreen = (errors: Result, res: Response) => {
+  const errorListData: GovUkErrorData[] = [];
+  let objectingEntityNameErr: GovUkErrorData | undefined = undefined;
+  let shareIdentityErr: GovUkErrorData | undefined = undefined;
+  errors.array({ onlyFirstError: true })
+    .forEach((valErr: ValidationError) => {
+      const govUkErrorData: GovUkErrorData = createGovUkErrorData(valErr.msg, "#" + valErr.param, true, "");
+      switch ((valErr.param)) {
+          case FULL_NAME_FIELD:
+            objectingEntityNameErr = govUkErrorData;
+            break;
+          case DIVULGE_INFO_FIELD:
+            shareIdentityErr = govUkErrorData;
+            break;
+      }
+      errorListData.push(govUkErrorData);
+    });
+  return res.render(Templates.OBJECTING_ENTITY_NAME, {
+    shareIdentityErr,
+    errorList: errorListData,
+    objectingEntityNameErr,
+    templateName: Templates.OBJECTING_ENTITY_NAME,
+  });
+}
