eaf860509080f7cde383d1981940675c35991909
markpit <mpitten@companieshouse.gov.uk>
2020-09-25 18:45:59 +0100

    Adding eslint rule for no unused vars/imports

--- src/controllers/error.controller.ts
+++ src/controllers/error.controller.ts
@@ -6,7 +6,7 @@ import logger from "../utils/logger";
  * This handler catches any other error/exception thrown within the application. Always keep this as the
  * last handler in the chain for it to work.
  */
-const errorHandler = (err, req: Request, res: Response, next: NextFunction) => {
+const errorHandler = (err, req: Request, res: Response, _next: NextFunction) => {
   logger.errorRequest(req, "An error has occurred. Re-routing to the error screen - " + err.stack);
   res.status(err.status || 500).render(Templates.ERROR);
 };
