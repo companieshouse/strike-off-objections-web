ac3e7d57ea5b717b52abc8eab0f30f428d25a035
Matthew Culpin <mculpin@companieshouse.gov.uk>
2020-10-27 13:20:27 +0000

    OBJ-250 shortend regex string

--- src/utils/string.formatter.ts
+++ src/utils/string.formatter.ts
@@ -2,7 +2,7 @@
  * String formatting functions
  */
 const REGEX_MATCH_NON_PRINTABLE_CHARS: RegExp =
-    /[^ÀÁÂÄÈÉÊËÌÍÎÏÒÓÔÖÙÚÛÜẀẂŴẄỲÝŶŸàáâäèéêëìíîïòóôöùúûüẁẃŵẅỳýŷÿABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789&@£$€¥.,:;–\-'''()\[\]{}<>!""»«"\?\/\\*=#%\+ \r\n]/g;
+    /[^ÀÁÂÄÈÉÊËÌÍÎÏÒÓÔÖÙÚÛÜẀẂŴẄỲÝŶŸàáâäèéêëìíîïòóôöùúûüẁẃŵẅỳýŷÿA-Za-z0-9&@£$€¥.,:;–\-‘’'()\[\]\{\}<>!“”»«"\?\/\\*=#%\+ \r\n]/g;
 
 export const removeNonPrintableChars = (inputStr: string): string => {
   if (inputStr) {
