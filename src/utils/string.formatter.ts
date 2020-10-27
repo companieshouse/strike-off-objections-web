565959a9375ac61ef7f8118bcf36edf91c9ae796
Matthew Culpin <mculpin@companieshouse.gov.uk>
2020-10-27 11:46:46 +0000

    OBJ-250 renamed vars

--- src/utils/string.formatter.ts
+++ src/utils/string.formatter.ts
@@ -1,12 +1,12 @@
 /*
  * String formatting functions
  */
-const REGEX_NON_PRINTABLE_CHARS: RegExp =
+const REGEX_MATCH_NON_PRINTABLE_CHARS: RegExp =
     /[^ÀÁÂÄÈÉÊËÌÍÎÏÒÓÔÖÙÚÛÜẀẂŴẄỲÝŶŸàáâäèéêëìíîïòóôöùúûüẁẃŵẅỳýŷÿABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789&@£$€¥.,:;–\-'''()\[\]{}<>!""»«"\?\/\\*=#%\+ \r\n]+/g;
 
 export const removeNonPrintableChars = (inputStr: string): string => {
   if (inputStr) {
-    return inputStr.replace(REGEX_NON_PRINTABLE_CHARS, " ");
+    return inputStr.replace(REGEX_MATCH_NON_PRINTABLE_CHARS, " ");
   } else {
     return inputStr;
   }
--- test/utils/string.formatter.spec.unit.ts
+++ test/utils/string.formatter.spec.unit.ts
@@ -3,36 +3,36 @@ import { removeNonPrintableChars } from "../../src/utils/string.formatter";
 
 describe("String formatter tests", () => {
   it("should not remove formatting chars", () => {
-    const dodgyString: string = "hello.\r\nThis string is not dodgy!";
-    const formattedString: string = removeNonPrintableChars(dodgyString);
+    const preFormattedString: string = "hello.\r\nThis string is not dodgy!";
+    const formattedString: string = removeNonPrintableChars(preFormattedString);
 
     expect(formattedString).toEqual("hello.\r\nThis string is not dodgy!");
   });
 
   it("should not remove accented character chars used in Welsh", () => {
-    const dodgyString: string = "Hylô amgáu düwch mẁg";
-    const formattedString: string = removeNonPrintableChars(dodgyString);
+    const preFormattedString: string = "Hylô amgáu düwch mẁg";
+    const formattedString: string = removeNonPrintableChars(preFormattedString);
 
     expect(formattedString).toEqual("Hylô amgáu düwch mẁg");
   });
 
   it("should not remove accented character chars used in Welsh in capitals", () => {
-    const dodgyString: string = "HYLÔ AMGÁU DÜWCH MẀG";
-    const formattedString: string = removeNonPrintableChars(dodgyString);
+    const preFormattedString: string = "HYLÔ AMGÁU DÜWCH MẀG";
+    const formattedString: string = removeNonPrintableChars(preFormattedString);
 
     expect(formattedString).toEqual("HYLÔ AMGÁU DÜWCH MẀG");
   });
 
   it("should remove non printable chars", () => {
-    const notDodgyString: string = "hello. This string is dodgy !`@\t£$%^&*()€#¢§¶ªº";
-    const formattedString: string = removeNonPrintableChars(notDodgyString);
+    const preFormattedString: string = "hello. This string is dodgy !`@\t£$%^&*()€#¢§¶ªº";
+    const formattedString: string = removeNonPrintableChars(preFormattedString);
 
     expect(formattedString).toEqual("hello. This string is dodgy ! @ £$% &*()€# ");
   });
 
   it("should not remove printable chars", () => {
-    const notDodgyString: string = "hello. This string is not dodgy !@£$%&*()€#";
-    const formattedString: string = removeNonPrintableChars(notDodgyString);
+    const preFormattedString: string = "hello. This string is not dodgy !@£$%&*()€#";
+    const formattedString: string = removeNonPrintableChars(preFormattedString);
 
     expect(formattedString).toEqual("hello. This string is not dodgy !@£$%&*()€#");
   });
