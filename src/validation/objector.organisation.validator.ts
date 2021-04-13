211fca9006abb6d17386314fa25ced5d7dd4b983
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2021-04-13 15:52:38 +0100

    Add validation logic

---
+++ src/validation/objector.organisation.validator.ts
@@ -0,0 +1,22 @@
+import { check, Result, ValidationError } from "express-validator";
+
+import { createGovUkErrorData } from "../model/govuk.error.data";
+import { ErrorMessages } from "../model/error.messages";
+
+const OBJECTOR_ORGANISATION = "objector-organisation";
+
+export const validators = [
+  check(OBJECTOR_ORGANISATION).not().isEmpty().withMessage(ErrorMessages.SELECT_OBJECTOR_ORGANISATION),
+];
+
+export const createErrorData = (errors: Result) => {
+  const errorText = errors.array()
+    .map((err: ValidationError) => err.msg)
+    .pop() as string;
+
+  return createGovUkErrorData(
+    errorText,
+    "#" + OBJECTOR_ORGANISATION,
+    true,
+    "");
+};
