96ddde9d6c2895a8c9454e00617aead481c69b7b
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2021-04-20 09:12:51 +0100

    Add unit test for objector organization page get method

--- test/controller/objector.organisation.controller.spec.unit.ts
+++ test/controller/objector.organisation.controller.spec.unit.ts
@@ -10,7 +10,7 @@ import request from "supertest";
 import { Session } from "@companieshouse/node-session-handler/lib/session/model/Session";
 
 import app from "../../src/app";
-import { OBJECTIONS_SESSION_NAME } from "../../src/constants";
+import { OBJECTIONS_SESSION_NAME, SESSION_OBJECTOR } from "../../src/constants";
 import { authenticationMiddleware } from "../../src/middleware/authentication.middleware";
 import { objectionSessionMiddleware } from "../../src/middleware/objection.session.middleware";
 import { sessionMiddleware } from "../../src/middleware/session.middleware";
@@ -19,12 +19,13 @@ import { OBJECTIONS_OBJECTING_ENTITY_NAME, OBJECTIONS_OBJECTOR_ORGANISATION } fr
 import { COOKIE_NAME } from "../../src/utils/properties";
 
 import { objectorOrganisation } from "../../src/validation";
-import { addToObjectionSession } from "../../src/services/objection.session.service";
+import { addToObjectionSession, retrieveFromObjectionSession } from "../../src/services/objection.session.service";
 
 const mockAuthenticationMiddleware = authenticationMiddleware as jest.Mock;
 const mockSessionMiddleware = sessionMiddleware as jest.Mock;
 const mockObjectionSessionMiddleware = objectionSessionMiddleware as jest.Mock;
 const mockSetObjectionSessionValue = addToObjectionSession as jest.Mock;
+const mockRetrieveFromObjectionSession = retrieveFromObjectionSession as jest.Mock;
 
 describe('objector organisation controller tests', () => {
   beforeEach(() => {
@@ -82,9 +83,39 @@ describe('objector organisation controller tests', () => {
       .set("Referer", "/")
       .set("Cookie", [`${COOKIE_NAME}=123`]);
 
-    // expect(mockObjectorOrganisationValidator).toHaveBeenCalledTimes(1);
     expect(response.status).toEqual(500);
     expect(mockCreateErrorData).toHaveBeenCalledTimes(1);
     expect(mockCreateErrorData).toThrow(Error("Throw Error on createErrorData"));
   });
+
+  it('should render objector organization page and verify the two radio-buttons', async () => {
+    mockSetObjectionSessionValue.mockReset();
+
+    const response = await request(app)
+      .get(OBJECTIONS_OBJECTOR_ORGANISATION)
+      .set("Referer", "/")
+      .set("Cookie", [`${COOKIE_NAME}=123`]);
+
+    expect(response.status).toEqual(200);
+    expect(response.text).toContain("myself-or-company");
+    expect(response.text).toContain("client");
+    expect(response.text).not.toContain(ErrorMessages.SELECT_OBJECTOR_ORGANISATION);
+  });
+
+  it('should catch and throw exception when retrieveFromObjectionSession', async () => {
+    mockRetrieveFromObjectionSession.mockReset();
+    mockRetrieveFromObjectionSession.mockImplementation(() => {
+      throw new Error("No Objection Session found in Session");
+    });
+
+    const response = await request(app)
+      .get(OBJECTIONS_OBJECTOR_ORGANISATION)
+      .set("Referer", "/")
+      .set("Cookie", [`${COOKIE_NAME}=123`]);
+
+    expect(response.status).toEqual(500);
+    expect(mockRetrieveFromObjectionSession).toHaveBeenCalledTimes(1);
+    expect(mockRetrieveFromObjectionSession).toThrow(Error("No Objection Session found in Session"));
+  });
+
 });
