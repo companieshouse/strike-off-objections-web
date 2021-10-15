95114ba5f09e0ede40978240d2d235363b82cb7a
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2021-10-15 16:09:22 +0100

    Add logic into strike off objections to integrate gov.uk start page

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -5,16 +5,26 @@ import { OBJECTIONS_SESSION_NAME } from "../constants";
 import { Templates } from "../model/template.paths";
 import logger from "../utils/logger";
 
-export const get = (req: Request, res: Response) => {
-  return res.render(Templates.INDEX, { templateName: Templates.INDEX });
-};
-
-export const post = (req: Request, res: Response) => {
+const deleteExtraData = (req) => {
   const session: Session | undefined = req.session as Session;
   if (session) {
     logger.debug("Deleting existing session data from " + OBJECTIONS_SESSION_NAME);
     session.deleteExtraData(OBJECTIONS_SESSION_NAME);
   }
+};
 
+export const get = (req: Request, res: Response) => {
+  const { start } = req.query;
+
+  if (start === "0") {
+    deleteExtraData(req);
+    return res.redirect(OBJECTIONS_OBJECTOR_ORGANISATION);
+  } else {
+    return res.render(Templates.INDEX, { templateName: Templates.INDEX });
+  }
+};
+
+export const post = (req: Request, res: Response) => {
+  deleteExtraData(req);
   return res.redirect(OBJECTIONS_OBJECTOR_ORGANISATION);
 };
