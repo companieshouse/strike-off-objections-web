96d75f4b01d5b4949493f65053f944184ece032c
dsanson <dsanson@companieshouse.gov.uk>
2022-07-19 14:26:30 +0100

    adding tests and log to improve coverage on top of implementation of whitelist to ensure change answers only redirects to safe urls

--- test/utils/request.util.spec.unit.ts
+++ test/utils/request.util.spec.unit.ts
@@ -6,8 +6,6 @@ import {
   getWhitelistedReturnToURL,
 } from "../../src/utils/request.util";
 
-
-import { Templates } from "../../src/model/template.paths";
 import { OBJECTIONS_OBJECTING_ENTITY_NAME, OBJECTIONS_ENTER_INFORMATION, OBJECTIONS_DOCUMENT_UPLOAD } from "../../src/model/page.urls";
 
 const UNKNOWN_URL = "/unknown";
@@ -33,29 +31,37 @@ describe("request.util.unit",
                const returnToUrl = extractValueIfPresentFromRequestField(UNKNOWN_URL, STRIKE_OFF_OBJECTIONS_DOCUMENT_UPLOAD);
                expect(returnToUrl).toEqual(null);
              });
+             it("returns null if asked to look up an blank page URL", () => {
+               const returnToUrl = extractValueIfPresentFromRequestField("", STRIKE_OFF_OBJECTIONS_DOCUMENT_UPLOAD);
+               expect(returnToUrl).toEqual(null);
+             });
+           });
+
+
+           describe("getWhitelistedReturnToURL", () => {
+             it("gets correct return to URL for Objecting Entity page", () => {
+               const returnToUrl = getWhitelistedReturnToURL(OBJECTIONS_OBJECTING_ENTITY_NAME);
+               expect(returnToUrl).toEqual(OBJECTIONS_OBJECTING_ENTITY_NAME);
+             });
+
+             it("gets correct return to URL for Enter Information page", () => {
+               const returnToUrl = getWhitelistedReturnToURL(OBJECTIONS_ENTER_INFORMATION);
+               expect(returnToUrl).toEqual(OBJECTIONS_ENTER_INFORMATION);
+             });
+
+             it("gets correct return to URL for Document Upload page", () => {
+               const returnToUrl = getWhitelistedReturnToURL(OBJECTIONS_DOCUMENT_UPLOAD);
+               expect(returnToUrl).toEqual(OBJECTIONS_DOCUMENT_UPLOAD);
+             });
+             it("errors if asked to look up an unknown page URL", () => {
+               const execution = () => getWhitelistedReturnToURL(UNKNOWN_URL);
+               expect(execution).toThrow("Return to URL /unknown not found in trusted URLs whitelist " +
+    "/\\/strike-off-objections\\/objecting-entity-name/," + "/\\/strike-off-objections\\/document-upload/," + "/\\/strike-off-objections\\/enter-information/.");
+             });
+             it("errors if asked to look up an blank page URL", () => {
+               const execution = () => getWhitelistedReturnToURL("");
+               expect(execution).toThrow("Return to URL  not found in trusted URLs whitelist " +
+    "/\\/strike-off-objections\\/objecting-entity-name/," + "/\\/strike-off-objections\\/document-upload/," + "/\\/strike-off-objections\\/enter-information/.");
+             });
            });
          });
-
-
-describe("getWhitelistedReturnToURL", () => {
-  it("gets correct return to URL for Objecting Entity page", () => {
-    const returnToUrl = getWhitelistedReturnToURL(OBJECTIONS_OBJECTING_ENTITY_NAME);
-    expect(returnToUrl).toEqual(OBJECTIONS_OBJECTING_ENTITY_NAME);
-  });
-
-  it("gets correct return to URL for Enter Information page", () => {
-    const returnToUrl = getWhitelistedReturnToURL(OBJECTIONS_ENTER_INFORMATION);
-    expect(returnToUrl).toEqual(OBJECTIONS_ENTER_INFORMATION);
-  });
-
-  it("gets correct return to URL for Document Upload page", () => {
-    const returnToUrl = getWhitelistedReturnToURL(OBJECTIONS_DOCUMENT_UPLOAD);
-    expect(returnToUrl).toEqual(OBJECTIONS_DOCUMENT_UPLOAD);
-  });
-  it("errors if asked to look up an unknown page URL", () => {
-    const execution = () => getWhitelistedReturnToURL(UNKNOWN_URL);
-    expect(execution).toThrow("Return to URL /unknown not found in trusted URLs whitelist " +
-    "/\\/strike-off-objections\\/objecting-entity-name/," + "/\\/strike-off-objections\\/document-upload/," + "/\\/strike-off-objections\\/enter-information/.");
-  });
-});
-
