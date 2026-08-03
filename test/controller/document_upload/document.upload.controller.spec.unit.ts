19054525d9244a6d35de1031dd9e0bdbace68877
burhanb99 <bboxwalla@companieshouse.gov.uk>
2026-08-03 16:58:02 +0100

    Remove unwanted comment

--- test/controller/document_upload/document.upload.controller.spec.unit.ts
+++ test/controller/document_upload/document.upload.controller.spec.unit.ts
@@ -375,7 +375,6 @@ describe ("document.document_upload.controller tests", () => {
     expect(response).not.toBeUndefined();
     expect(response.text).toContain(INVALID_MIME_TYPE);
     expect(mockAddAttachment).toHaveBeenCalled();
-    // done();
   });
 
   it ("AJAX - should render error message on 415 error", async () => {
@@ -411,7 +410,6 @@ describe ("document.document_upload.controller tests", () => {
 
     expect(response.text).toContain(INVALID_MIME_TYPE);
     expect(mockAddAttachment).toHaveBeenCalled();
-    // done();
   });
 
   it("should have correct form action fields on document upload page", async () => {
